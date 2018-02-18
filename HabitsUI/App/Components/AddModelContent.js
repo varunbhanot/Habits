52914073e43fa999566ad8d965fa9446053fb110
varun bhanot <varunbhanot@gmail.com>
2018-02-18 20:16:44 +0530

    Modal UI

--- HabitsUI/App/Components/AddModelContent.js
+++ HabitsUI/App/Components/AddModelContent.js
@@ -1,40 +1,51 @@
 import React, { Component } from 'react'
 // import PropTypes from 'prop-types';
-import { View, Text,TouchableOpacity,TextInput } from 'react-native'
+import { View, Text, TouchableOpacity, Dimensions } from 'react-native'
 import styles from './Styles/AddModelContentStyle'
-import Modal from "react-native-modal";
+import Modal from 'react-native-modal'
+import { FormInput,Header } from 'react-native-elements'
+
+
+const SCREEN_WIDTH = Dimensions.get('window').width;
 
 export default class AddModelContent extends Component {
   constructor(props) {
     super(props);
     this.state = { text: 'Habits Name' };
   }
-    _renderButton = (text, onPress) => (
-      <TouchableOpacity onPress={onPress}>
-        <View style={styles.button}>
-          <Text>{text}</Text>
-        </View>
-      </TouchableOpacity>
-    );
-  
-    _renderModalContent = (modalToggle) => (
-      <View style={styles.modalContent}>
-        <Text>Hello!</Text>
-        <TextInput
-        style={{height: 40, borderColor: 'gray', borderWidth: 1}}
-        onChangeText={(text) => this.setState({text})}
-        value={this.state.text}
-      />
-        {this._renderButton('Close', ()=>modalToggle(true))}
+  _renderButton = (text, onPress) => (
+    <TouchableOpacity onPress={onPress}>
+      <View >
+        <Text style={styles.button} >{text}</Text>
       </View>
-    );
+    </TouchableOpacity>
+  );
+
+  _renderModalContent = (modalToggle) => (
+    <View style={styles.modalContent}>
+      <View style={styles.headerContainer }>
+        <Text style={styles.headerText}>Create Habit</Text>
+    </View>  
+    <View style={styles.inputContainer}>
+      <FormInput containerStyle={{ borderRadius: 40, borderWidth: 1, borderColor: "#26A69A", height: 50, width: SCREEN_WIDTH - 90, marginVertical: 10 }}
+        placeholder="Name" placeholderTextColor="#26A69A" inputStyle={{ marginLeft: 15, color: "white" }} autoCapitalize="none" autoCorrect={false}
+        keyboardAppearance="light" returnKeyType="next" blurOnSubmit={false}  ref={input => (this.usernameInput = input)}/>
+      <FormInput containerStyle={{ borderRadius: 40, borderWidth: 1, borderColor: "#26A69A", height: 50, width: SCREEN_WIDTH - 90, marginVertical: 10 }}
+        placeholder="Question (Did you .. today?)" placeholderTextColor="#26A69A" inputStyle={{ marginLeft: 15, color: "white" }} autoCapitalize="none" autoCorrect={false}
+        keyboardAppearance="light" returnKeyType="next" blurOnSubmit={false} />
+        </View>
+      <View style={styles.buttonContainer}>
+      {this._renderButton('Discard', () => modalToggle(true))}
+      {this._renderButton('Save', () => saveHabit(true))}
+      </View>
+    </View>    
+  );
 
   render() {
-    
     return (
       <View style={{ flex: 1 }}>
         <Modal isVisible={this.props.isModalVisible}>
-          {this._renderModalContent(this.props.modalToggle)}
+          {this._renderModalContent(this.props.modalToggle,this.props.saveHabit)}
         </Modal>
       </View>
     );
--- HabitsUI/App/Components/Styles/AddModelContentStyle.js
+++ HabitsUI/App/Components/Styles/AddModelContentStyle.js
@@ -6,23 +6,43 @@ export default StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-  },
-  button: {
-    backgroundColor: 'lightblue',
-    padding: 12,
+  },  
+  button: {    
     margin: 16,
+    fontFamily: 'Lato-Regular',
+    fontSize:15,
+    color:'#ffffff'
+  },
+  headerText:{
+    fontFamily: 'Lato-Regular',
+    fontSize: 25,
+    color: '#ffffff'
+  },
+  headerContainer:{
+    justifyContent: 'flex-start',
+    alignItems:'flex-start',
+    marginBottom:10
+
+  },
+  inputContainer:{
     justifyContent: 'center',
-    alignItems: 'center',
-    borderRadius: 4,
-    borderColor: 'rgba(0, 0, 0, 0.1)',
+    alignItems: 'center'  
+  },
+  buttonContainer:{    
+    flexDirection: 'row',    
+    justifyContent: 'flex-end',
+    alignItems: 'flex-end'        
+
   },
   modalContent: {
     backgroundColor: '#282727',
     padding: 22,
-    justifyContent: 'center',
-    alignItems: 'center',
     borderRadius: 4,
     borderColor: 'rgba(0, 0, 0, 0.1)',
+  },
+  modalData :{
+    justifyContent: 'flex-start',    
+    marginLeft:10
   },
   bottomModal: {
     justifyContent: 'flex-end',
